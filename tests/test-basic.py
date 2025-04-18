89851d5f75deea69510e95d17b412e181b79738f
Ezeagu Prince <72462776+Enyinnaya-alt@users.noreply.github.com>
2025-04-18 14:05:07 +0100

    test

--- tests/test-basic.py
+++
@@ -1,4 +0,0 @@
-# tests/test_basic.py
-
-def test_sanity_check():
-    assert 14 + 14 == 28
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,4 @@
 # tests/test_basic.py
 
 def test_sanity_check():
-    assert 14 + 14 == 28
+    assert 1 + 1 == 2
